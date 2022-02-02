fb69426f142c9977bc8943a1eed4a6dc0a048e2d
nonitut <nona.blogg@gmail.com>
2022-02-02 15:14:33 +0300

    123

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,1 +1,17 @@
-//here js
+
+
+let x;
+$(window).on('scroll', function() {
+	x = $(window).scrollTop();
+	$('.test').css('background-size', 100 + parseInt(x / 10) + '% ');
+}); // обозначаем перееменную, обращаемся к вьюпорту
+//scrollTop() -  величина в позиции 0, когда эллемент в начальной позиции
+//обращаемся ко всем дивам и работаем со всеми его background-size
+// parseInt - работа с целым значением
+
+
+let y;
+$(window).on('scroll', function() {
+	y = $(window).scrollTop();
+	$('.kubik').css('left', 0 + parseInt(y-2) + '%');
+});
